0c677b14076c7681ab1f80a43cc1170450985830
milon-hossain <abireshan32@gmial.com>
2024-04-30 02:59:28 +0600

    change the environment variable

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app=express()
 const cors = require('cors');
 const port=process.env.PORT||5000
 require('dotenv').config()
-//middleWare
 app.use(cors())
 app.use(express.json())
 
@@ -13,7 +12,7 @@ console.log(process.env.USER_NAME);
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASS}@cluster0.pnsxsk9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -106,7 +105,7 @@ app.delete("/places/:id",async(req,res)=>{
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // await client.close();
+   
   }
 }
 run().catch(console.dir);
